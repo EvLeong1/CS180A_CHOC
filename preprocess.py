e645d1aad1e64586f7b8f1bf2446548e2db33007
Marcus Linture <mlinture@uci.edu>
2024-04-09 09:42:41 -0700

    Added graphs to show rate of change of hemoglobin

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@
 # pip install openpyxl
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.read_excel('capstone_dataset.xlsx')
 
@@ -20,4 +21,13 @@ def hb_roc(row):
     return np.mean(rates) if rates else np.nan
 
 hb_stats['avg_rate_of_change'] = hb_df.apply(hb_roc, axis=1)
-print(hb_stats['avg_rate_of_change'])
+
+def graph_change(row):
+    graph = []
+    for i, j in zip(range(0, len(row)-1, 2), range(1, len(row), 2)):
+        if not pd.isnull(row[i]) and not pd.isnull(row[j]) and row[i]!=999.0 and row[j]!=999.0 and row[i]!=888.0 and row[j]!=888.0:
+            graph.append((row[i], row[j]))
+    plt.plot(graph)
+
+hemoglobin_levels.apply(graph_change, axis=1)
+plt.show()
